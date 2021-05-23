a0fefba20c0ab57852b775779ce40ae9edd88957
Marcos Santana <marcos.santana@meta.com.br>
2021-05-23 20:12:16 -0300

    feat: add log on application stars

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
 import { Application } from './main/app';
+import { logger } from './main/app/logger';
 
 const application = new Application();
 
 setImmediate(async () => {
   await application.start();
+  logger.info('Application started');
 });
