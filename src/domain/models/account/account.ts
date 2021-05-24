f18953243bbf5de7f7cf3027d1ca5e55cb8cd0e8
Marcos Santana <marcos.santana@meta.com.br>
2021-05-24 20:52:43 -0300

    fix: update roles prop type

--- src/domain/models/account/account.ts
+++ src/domain/models/account/account.ts
@@ -1,11 +1,9 @@
-import { Role } from '../role';
-
 export type Account = {
   _id: string;
   name: string;
   email: string;
   password: string;
-  roles: Role[];
+  roles: string[];
   created_at: Date;
   updated_at: Date;
   deleted_at: Date | null;
