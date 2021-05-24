4f704b47e28153951b3503d007cd0730ad7fb6ff
Marcos Santana <marcos.santana@meta.com.br>
2021-05-24 16:48:55 -0300

    feat: add cryptography layer

---
+++ src/infra/cryptography/adapters/bcrypt-adapter.ts
@@ -0,0 +1,14 @@
+import bcrypt from 'bcrypt';
+import { HashComparer, Hasher } from '../protocols';
+
+export class BcryptAdapter implements Hasher, HashComparer {
+  constructor(private readonly salt: number) {}
+
+  async hash(plaintext: string): Promise<string> {
+    return bcrypt.hash(plaintext, this.salt);
+  }
+
+  async compare(plaintext: string, digest: string): Promise<boolean> {
+    return bcrypt.compare(plaintext, digest);
+  }
+}
