c17fa63736153f5ab00bc33e99525b7f5d23c3a0
Marcos Santana <marcos.santana@meta.com.br>
2021-05-25 13:26:07 -0300

    chore: change nodemon for ts-node-dev

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,13 @@
 module.exports = {
-  roots: ['<rootDir>/test'],
+  roots: ['<rootDir>/tests'],
   collectCoverageFrom: ['<rootDir>/src/**/*.ts', '!<rootDir>/src/main/**'],
   coverageDirectory: 'coverage',
   testEnvironment: 'node',
   transform: {
     '.+\\.ts$': 'ts-jest',
+  },
+  moduleNameMapper: {
+    '@/tests/(.*)': '<rootDir>/tests/$1',
+    '@/(.*)': '<rootDir>/src/$1',
   },
 };
