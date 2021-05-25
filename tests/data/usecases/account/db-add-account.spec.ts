22fe1ea9420e2ffd0de3646998256d3a739474d2
Marcos Santana <marcos.santana@meta.com.br>
2021-05-25 14:34:08 -0300

    test: add DbAddAccount test

---
+++ tests/data/usecases/account/db-add-account.spec.ts
@@ -0,0 +1,33 @@
+import { DbAddAccount } from '@/data/usecases/account';
+import { BcryptAdapter } from '@/infra/cryptography/adapters';
+import { MemoryAccountRepository } from '@/infra/db/account/repositories/memory/memory-account-repository';
+import { MemoryRoleRepository } from '@/infra/db/role/repositories/memory/memory-role-repository';
+
+describe('DbAddAccount', () => {
+  beforeAll(() => {
+    jest.resetModules();
+    process.env.CRYPTOGRAPHY_SALT = '12';
+  });
+  it('Should add an account to db', async () => {
+    const hasher = new BcryptAdapter();
+    const accountRepository = new MemoryAccountRepository();
+    const roleRepository = new MemoryRoleRepository();
+    const dbAddAccount = new DbAddAccount(
+      hasher,
+      accountRepository,
+      roleRepository
+    );
+
+    await dbAddAccount.add({
+      name: 'any_name',
+      email: 'any_email',
+      roles: [],
+      password: 'any_password',
+    });
+    const account = await accountRepository.findByEmail('any_email');
+
+    expect(account?.name).toEqual('any_name');
+    expect(account?.email).toEqual('any_email');
+    expect(account?.roles).toEqual([]);
+  });
+});
