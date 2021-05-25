1cf68d76bd06969dde0520ace80f56284a8619c9
Marcos Santana <marcos.santana@meta.com.br>
2021-05-24 21:04:56 -0300

    feat: add usecases on container injection

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -1,4 +1,8 @@
 import { container } from 'tsyringe';
+import { AddAccount } from '../../domain/usecases/add-account';
+import { DbAddAccount } from '../../data/usecases/db-add-account';
+import { AddRole } from '../../domain/usecases/add-role';
+import { DbAddRole } from '../../data/usecases/db-add-role';
 import { Hasher } from '../../infra/cryptography/protocols';
 import { BcryptAdapter } from '../../infra/cryptography/adapters';
 import { RoleRepository } from '../../infra/db/role/repositories/role-repository';
@@ -10,10 +14,12 @@ container.registerSingleton<AccountRepository>(
   'AccountRepository',
   MongoAccountRepository
 );
-
 container.registerSingleton<RoleRepository>(
   'RoleRepository',
   MongoRoleRepository
 );
+
+container.registerSingleton<AddAccount>('AddAccount', DbAddAccount);
+container.registerSingleton<AddRole>('AddRole', DbAddRole);
 
 container.registerSingleton<Hasher>('Hasher', BcryptAdapter);
