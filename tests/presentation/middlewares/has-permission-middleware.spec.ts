9f118dbe5b96a7d6102d8546de804c2bb448b304
Marcos Santana <marcos.santana@meta.com.br>
2021-05-25 13:54:34 -0300

    test: add hasPermission middleware test

---
+++ tests/presentation/middlewares/has-permission-middleware.spec.ts
@@ -0,0 +1,34 @@
+import { HttpRequest } from '@/presentation/protocols';
+import { hasPermission } from '@/presentation/middlewares/has-permission-midddleware';
+
+describe('hasPermissionMiddleware', () => {
+  it('Should return statusCode 200 if user has permission', async () => {
+    const necessary_permissions = ['role_1', 'role_2'];
+    const request: HttpRequest = {
+      user: {
+        id: 'any_id',
+        roles: ['role_1', 'role_2', 'role_3'],
+      },
+    };
+
+    const permissionMiddleware = hasPermission(necessary_permissions);
+    const response = await permissionMiddleware(request);
+
+    expect(response.statusCode).toEqual(200);
+  });
+
+  it('Should return statusCode 401 if user has no permission', async () => {
+    const necessary_permissions = ['role_1', 'role_2'];
+    const request: HttpRequest = {
+      user: {
+        id: 'any_id',
+        roles: ['role_1', 'role_3'],
+      },
+    };
+
+    const permissionMiddleware = hasPermission(necessary_permissions);
+    const response = await permissionMiddleware(request);
+
+    expect(response.statusCode).toEqual(401);
+  });
+});
