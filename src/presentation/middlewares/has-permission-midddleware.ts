c4c878eb818d7d00e325fcf1c995f3cbd94a407a
Marcos Santana <marcos.santana@meta.com.br>
2021-05-25 12:04:00 -0300

    feat: add hasPermission middleware

---
+++ src/presentation/middlewares/has-permission-midddleware.ts
@@ -0,0 +1,21 @@
+import { Unauthorized } from '../errors';
+import { ok, unauthorized } from '../helper';
+import { HttpRequest, HttpResponse, Middleware } from '../protocols';
+
+const includesAllItems = (arr: string[], target: string[]): boolean => {
+  return target.every(v => arr.includes(v));
+};
+
+export const hasPermission =
+  (authRoles: string[]): Middleware =>
+  async (httpRequest: HttpRequest): Promise<HttpResponse> => {
+    try {
+      const roles = httpRequest.user?.roles as string[];
+      if (!includesAllItems(roles, authRoles))
+        throw new Unauthorized('You have no permission', 'Unauthorized');
+
+      return Promise.resolve(ok({}));
+    } catch (error) {
+      return unauthorized(error);
+    }
+  };
