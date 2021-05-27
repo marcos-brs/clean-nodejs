40a710f7757a0852d45b3b4f19cdd89d56041ed0
Marcos Santana <marcos.santana@meta.com.br>
2021-05-27 15:41:21 -0300

    fix: pass the error to errorHandlerMiddleware

--- src/main/adapters/express-controller.ts
+++ src/main/adapters/express-controller.ts
@@ -3,20 +3,24 @@ import { HttpRequest, Controller } from '../../presentation/protocols';
 
 export const ExpressControllerAdapter = (controller: Controller) => {
   return async (req: Request, res: Response, next: NextFunction) => {
-    const httpRequest: HttpRequest = {
-      body: req.body,
-      params: req.params,
-      query: req.query,
-      headers: req.headers,
-    };
+    try {
+      const httpRequest: HttpRequest = {
+        body: req.body,
+        params: req.params,
+        query: req.query,
+        headers: req.headers,
+      };
 
-    const httpResponse = await controller(httpRequest);
-    if (httpResponse.statusCode >= 200 && httpResponse.statusCode <= 299) {
-      res.status(httpResponse.statusCode).json(httpResponse.body);
-    } else {
-      res.status(httpResponse.statusCode).json({
-        error: httpResponse.body.message,
-      });
+      const httpResponse = await controller(httpRequest);
+      if (httpResponse.statusCode >= 200 && httpResponse.statusCode <= 299) {
+        res.status(httpResponse.statusCode).json(httpResponse.body);
+      } else {
+        res.status(httpResponse.statusCode).json({
+          error: httpResponse.body.message,
+        });
+      }
+    } catch (error) {
+      next(error);
     }
   };
 };
