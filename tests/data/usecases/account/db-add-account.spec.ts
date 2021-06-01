5827bf75044199aae6a3323da9e3365d06dee3d0
Marcos Santana <marcos.santana@meta.com.br>
2021-06-01 12:02:11 -0300

    fix: add uuid into constructor

--- tests/data/usecases/account/db-add-account.spec.ts
+++ tests/data/usecases/account/db-add-account.spec.ts
@@ -2,6 +2,7 @@ import { DbAddAccount } from '@/data/usecases/account';
 import { BcryptAdapter } from '@/infra/cryptography/adapters';
 import { MemoryAccountRepository } from '@/infra/db/account/repositories';
 import { MemoryRoleRepository } from '@/infra/db/role/repositories';
+import { Uuidv4Adapter } from '@/infra/uuid/adapters';
 
 describe('DbAddAccount', () => {
   beforeAll(() => {
@@ -10,10 +11,12 @@ describe('DbAddAccount', () => {
   });
   it('Should add an account to db', async () => {
     const hasher = new BcryptAdapter();
+    const uuid = new Uuidv4Adapter();
     const accountRepository = new MemoryAccountRepository();
     const roleRepository = new MemoryRoleRepository();
     const dbAddAccount = new DbAddAccount(
       hasher,
+      uuid,
       accountRepository,
       roleRepository
     );
--- tests/data/usecases/role/db-add-role.spec.ts
+++ tests/data/usecases/role/db-add-role.spec.ts
@@ -1,10 +1,12 @@
 import { DbAddRole } from '@/data/usecases/role';
 import { MemoryRoleRepository } from '@/infra/db/role/repositories';
+import { Uuidv4Adapter } from '@/infra/uuid/adapters';
 
 describe('DbAddRole', () => {
   it('Should add an role to db', async () => {
     const roleRepository = new MemoryRoleRepository();
-    const dbAddRole = new DbAddRole(roleRepository);
+    const uuid = new Uuidv4Adapter();
+    const dbAddRole = new DbAddRole(uuid, roleRepository);
 
     await dbAddRole.add({
       role: 'any_role',
