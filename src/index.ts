d1a9f9759b68f381d79187c73cee4c2561b982a1
Marcos Santana <brmarcossantanabr@gmail.com>
2021-09-01 15:17:22 -0300

    fix: configure mudule alias

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,11 @@
+/* eslint-disable import/first */
+/* eslint-disable import/newline-after-import */
 import 'reflect-metadata';
 import 'module-alias/register';
+import moduleAlias from 'module-alias';
+moduleAlias.addAliases({
+  '@': `${__dirname}`,
+});
 import { Application } from './main/app';
 import { logger } from './main/app/logger';
 import './shared/container';
