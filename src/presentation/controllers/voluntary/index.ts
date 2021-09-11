3b26590df0c998a20fed8a70dcc77a06b7744661
Marcos Santana <brmarcossantanabr@gmail.com>
2021-09-11 16:03:19 -0300

    feat: add delete voluntary route

--- src/presentation/controllers/voluntary/index.ts
+++ src/presentation/controllers/voluntary/index.ts
@@ -1,11 +1,16 @@
 import { inject, injectable } from 'tsyringe';
-import { AddVoluntary, UpdateVoluntary } from '@/domain/usecases/voluntary';
-import { Controller, Patch, Post } from '@/presentation/decorators';
+import {
+  AddVoluntary,
+  DeleteVoluntary,
+  UpdateVoluntary,
+} from '@/domain/usecases/voluntary';
+import { Controller, Delete, Patch, Post } from '@/presentation/decorators';
 import { ok } from '@/presentation/helper';
 import { validatorMiddleware } from '@/presentation/middlewares';
 import { HttpRequest, HttpResponse } from '@/presentation/protocols';
 import { BaseController } from '@/presentation/protocols/controller';
 import { signupSchema, updateSchema } from './schemas';
+import { deleteSchema } from '../student/schemas';
 
 @injectable()
 @Controller('/voluntary')
@@ -14,7 +19,9 @@ export class VoluntaryController extends BaseController {
     @inject('UpdateVoluntary')
     private updateVoluntary: UpdateVoluntary,
     @inject('AddVoluntary')
-    private addVoluntary: AddVoluntary
+    private addVoluntary: AddVoluntary,
+    @inject('DeleteVoluntary')
+    private deleteVoluntary: DeleteVoluntary
   ) {
     super();
   }
@@ -29,6 +36,13 @@ export class VoluntaryController extends BaseController {
   @Post('/signup', [validatorMiddleware(signupSchema)])
   async createVoluntary(req: HttpRequest): Promise<HttpResponse> {
     const response = await this.addVoluntary.add(req.body);
+
+    return ok(response);
+  }
+
+  @Delete('/delete', [validatorMiddleware(deleteSchema)])
+  async destroy(req: HttpRequest): Promise<HttpResponse> {
+    const response = await this.deleteVoluntary.delete(req.body);
 
     return ok(response);
   }
