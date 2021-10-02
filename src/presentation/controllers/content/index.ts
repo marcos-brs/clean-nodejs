767948fabe24147df056f53af3a7a9133af05e6e
Darlan-Freire <darlan.freirerj@gmail.com>
2021-10-02 00:14:49 -0300

    feat: add delete-content to controller

--- src/presentation/controllers/content/index.ts
+++ src/presentation/controllers/content/index.ts
@@ -1,11 +1,12 @@
-import { AddContent } from '@/domain/usecases/content';
+import { AddContent, DeleteContent } from '@/domain/usecases/content';
 import { JwtAdapter } from '@/infra/cryptography/adapters/jwt-adapter';
 import { addTokenToRequest } from '@/presentation/middlewares';
 import { injectable, inject, container } from 'tsyringe';
-import { Controller, Post } from '../../decorators';
+import { Controller, Delete, Post } from '../../decorators';
 import { ok } from '../../helper';
 import { validatorMiddleware } from '../../middlewares/validator-middleware';
 import { BaseController, HttpRequest, HttpResponse } from '../../protocols';
+import { deleteContentSchema } from './schemas';
 import { createContentSchema } from './schemas/create-content-schema';
 
 @injectable()
@@ -13,12 +14,14 @@ import { createContentSchema } from './schemas/create-content-schema';
 export class ContentController extends BaseController {
   constructor(
     @inject('AddContent')
-    private addContent: AddContent
+    private addContent: AddContent,
+    @inject('DeleteContent')
+    private deleteContent: DeleteContent
   ) {
     super();
   }
 
-  @Post('/', [
+  @Post('/a', [
     validatorMiddleware(createContentSchema),
     addTokenToRequest(container.resolve(JwtAdapter)),
   ])
@@ -32,6 +35,20 @@ export class ContentController extends BaseController {
     };
 
     const response = await this.addContent.add(content);
+
+    return ok(response);
+  }
+
+  @Delete('/delete', [
+    validatorMiddleware(deleteContentSchema),
+    addTokenToRequest(container.resolve(JwtAdapter)),
+  ])
+  async delete(req: HttpRequest): Promise<HttpResponse> {
+    const content: DeleteContent.Params = {
+      destination_url: req.body.destination_url,
+    };
+
+    const response = await this.deleteContent.delete(content);
 
     return ok(response);
   }
--- src/presentation/controllers/content/schemas/index.ts
+++ src/presentation/controllers/content/schemas/index.ts
@@ -1,1 +1,2 @@
 export * from './create-content-schema';
+export * from './delete-content-schema';
